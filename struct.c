aa8d93abaa2c798b32887bf63de72b77f48483c5
Leandro S. Araújo <leandro_sy@hotmail.com>
2016-07-11 13:04:11 -0300

    Update struct.c

--- struct.c
+++ struct.c
@@ -8,10 +8,16 @@ struct Data {
     char str[80];
 };
 
+void useData(struct Data *data){
+    data->price = 90;
+};
+
 int main(){
     struct Data data;
     printf( "Memory size occupied by data : %d\n", sizeof(data));
     data.price = 80;
+    printf("The price is %d \n", data.price);
+    useData(&data);
     printf("The price is %d \n", data.price);
     return 0;
 };
