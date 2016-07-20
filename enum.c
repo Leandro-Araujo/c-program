b0bbc597e029125b2d29daa94de55160410ce98d
Leandro S. Araújo <leandro_sy@hotmail.com>
2016-07-20 10:57:19 -0300

    Create enum.c

---
+++ enum.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <string.h>
+
+enum {Janeiro = 1, Fevereiro, Marco, Abril, Maio, Junho, Julho, Agosto, Setembro, Outubro, Novembro, Dezembro};
+
+int main(){
+  printf("%d\n", Dezembro);
+};
